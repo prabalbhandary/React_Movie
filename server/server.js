5390d9377cd4fd64f81d0df56d78c6bfecd59603
prabalbhandary <bhandaryprabal837@gmail.com>
2025-11-21 21:57:57 +0545

    added servecr to the folder

---
+++ server/server.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import "colors";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import movieRoutes from "./routes/movieRoutes.js";
+
+const app = express();
+
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/movies", movieRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server running on http://localhost:${port}`.bgCyan.white);
+});
